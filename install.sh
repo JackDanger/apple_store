87a5b6ebc6bee9aa50069a0d375d7dee65b92605
Jack Danger Canty <studioda@Johanna.local>
2009-02-02 12:30:58 -0800

    adding basic install script.

---
+++ install.sh
@@ -0,0 +1,23 @@
+
+mkdir -p _work
+cd _work
+
+## Retrieve Git
+# curl -Ov http://git-osx-installer.googlecode.com/files/git-1.6.1.1-intel-leopard.dmg
+cp ../../git.dmg ./git-1.6.1.1-intel-leopard.dmg
+
+## Install Git from the DMG file
+hdiutil attach git*.dmg
+cp /Volumes/Git-1.6.1.1-Intel-Leopard/git-1.6.1.1-intel-leopard.pkg ./
+hdiutil detach /Volumes/Git-*Intel-Leopard/
+sudo installer -pkg git*.pkg -target "/"
+
+## Install Rubygems from source
+curl -O http://rubyforge.org/frs/download.php/45905/rubygems-1.3.1.tgz
+tar -xvf rubygems*.tgz
+rm rubygems.tgz
+cd rubygems*
+ruby setup.rb
+
+
+
