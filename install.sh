1b77819f2a01a19b83254849b573583020205c79
Apple <apple@ars072-60.local>
2009-02-03 15:26:35 -0800

    Fixing rubygems local install.

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@ HERE=`pwd`
 mkdir -p $HERE/src
 mkdir -p $HERE/bin
 mkdir -p $HERE/git
-mkdir -p $HERE/gems
+mkdir -p $HERE/.gems
 
 ## Setup local environment
 cat > $HERE/bash_env <<-EOF
-export GEM_HOME="$HERE/gems"
+export GEM_HOME="$HERE/.gems"
 export GEM_PATH="$GEM_HOME:/usr/lib/ruby/gems/1.8"
-export PATH="$HERE/bin:$HERE/git/bin:$HERE/git/libexec/git-core/:$HERE/gems/bin:$PATH"
+export PATH="$HERE/bin:$HERE/git/bin:$HERE/git/libexec/git-core/:$HERE/.gems/bin:$PATH"
 EOF
 source $HERE/bash_env
 
@@ -19,6 +19,7 @@ cd $HERE/src
 curl -OL http://git-osx-installer.googlecode.com/files/git-1.6.1.1-intel-leopard.dmg
 
 ## Extract Git binaries from the DMG file
+cd $HERE/src
 hdiutil attach git*.dmg
 cp /Volumes/Git-1.6.1.1-Intel-Leopard/git-1.6.1.1-intel-leopard.pkg ./
 hdiutil detach /Volumes/Git-*Intel-Leopard/
@@ -30,12 +31,15 @@ rm -rf System Resources Distribution etc.pkg git.pkg
 ## Installing Git to $HERE/git
 mv lib libexec man share contrib bin $HERE/git/
 
+
 ## Install Rubygems from source
+cd $HERE/src
 curl -OLv http://rubyforge.org/frs/download.php/45905/rubygems-1.3.1.tgz
 tar -xvf rubygems*.tgz
 rm rubygems.tgz
 cd rubygems*
-ruby setup.rb
+ruby setup.rb --prefix=$HERE >/dev/null
+echo "You're using RubyGems version `gem -v`"
 
 
 
